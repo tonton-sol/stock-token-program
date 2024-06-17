70a26f70acd2405388f65429e5d0d6a70cd7beb5
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-06-17 13:07:47 +0900

    find_nth_weekday_in_month now takes a Weekday

--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,8 @@ use chrono::{DateTime, Datelike, FixedOffset, TimeZone, Timelike, Utc, Weekday};
 
 fn is_daylight_saving_time(datetime: DateTime<Utc>) -> bool {
     let year = datetime.year();
-    let dst_start = find_nth_weekday_in_month(year, 3, 0, 2);
-    let dst_end = find_nth_weekday_in_month(year, 11, 0, 1);
+    let dst_start = find_nth_weekday_in_month(year, 3, Weekday::Sun, 2);
+    let dst_end = find_nth_weekday_in_month(year, 11, Weekday::Sun, 1);
 
     let dst_start_utc = Utc.with_ymd_and_hms(year, 3, dst_start, 7, 0, 0).unwrap();
     let dst_end_utc = Utc.with_ymd_and_hms(year, 11, dst_end, 6, 0, 0).unwrap();
@@ -23,14 +23,14 @@ fn convert_utc_to_et(utc_datetime: DateTime<Utc>) -> DateTime<FixedOffset> {
     utc_datetime.with_timezone(&offset)
 }
 
-fn find_nth_weekday_in_month(year: i32, month: u32, weekday: u32, nth: u32) -> u32 {
+fn find_nth_weekday_in_month(year: i32, month: u32, weekday: Weekday, nth: u32) -> u32 {
     let mut count = 0;
     for day in 1..32 {
         let date = Utc.with_ymd_and_hms(year, month, day, 0, 0, 0).unwrap();
         if date.month() != month {
             break; // No more days in the month
         }
-        if date.weekday().num_days_from_sunday() as u32 == weekday {
+        if date.weekday() == weekday {
             count += 1;
             if count == nth {
                 return day;
